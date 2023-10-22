48fd6e63dcce6e0d9c45f3c0e4053365172e3ea0
Deric Le <rake@tamu.edu>
2023-10-21 20:37:20 -0500

    add helpful comments for development in landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,9 +19,19 @@ function App() {
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="Sweet Paris logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the landing (App) page. <br /> <strong>Please read:</strong>
+          <br />
+          - See App.css for css file organizing.
+          <br />
+          - See file structure (components) for adding new features in an organized manner.
+          <br /> 
+          - See "backend/connection.js" and "backend/routes/index.js" for creating and releasing connections.
+          <br /> 
+          - See pinned Google Drive link in Discord for recommended working environment.
         </p>
+
         <button onClick={goToLoginPage}>Go to Login Page</button>
         <button onClick={goToMenuPage}>Go to Menu Page (temporary)</button>
 
