375c24c9dc9c8bd8cf42773f23b39fdf49983849
Deric Le <rake@tamu.edu>
2023-10-25 11:34:02 -0500

    successfully validate credentials on backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,37 +6,34 @@ var { loginController, registerController } = require('../controllers/userContro
 
 // responds to localhost:3001/users/login request
 router.post('/login', async (req, res) => {
-  console.log(req.body);
-  // const { email, password } = req.body;
+  const { email, password } = req.body;
 
-  // try {
-  //   const conn = await connection.connect();
-  //   console.log('created connection');
+  try {
+    console.log("email: " + email + " password: " + password);
+    const conn = await connection.connect();
+    console.log('created connection');
 
-  //   const employeeCredentials = await conn.query
-  //     ('SELECT * FROM employees WHERE email = $1 AND password = $2', [email, password]);
-  //   const managerCredentials = await conn.query
-  //     ('SELECT * FROM managers WHERE email = $1 AND password = $2', [email, password]);
+    const employeeCredentials = await conn.query
+      ('SELECT * FROM employees WHERE email = $1 AND password = $2', [email, password]);
+    const managerCredentials = await conn.query
+      ('SELECT * FROM managers WHERE email = $1 AND password = $2', [email, password]);
 
-  //   conn.release(); 
-  //   console.log('closed connection');
+    conn.release(); 
+    console.log('closed connection');
 
-  //   if (employeeCredentials.rowCount > 0) {
-  //     res.json({ isEmployee: true });
-  //   } else if (managerCredentials.rowCount > 0) {
-  //     res.json({ isManager: true });
-  //   } else {
-  //     res.json({ isEmployee: false, isManager: false });
-  //   }
+    if (employeeCredentials.rowCount > 0) {
+      res.json({ isManager: false, isEmployee: true });
+    } else if (managerCredentials.rowCount > 0) {
+      res.json({ isManager: true, isEmployee: false });
+    } else {
+      res.json({ isEmployee: false, isManager: false });
+    }
 
-  // } catch (error) {
-  //   console.error('Error connecting to the database:', error);
-  //   res.status(500).json({ error: 'Internal Server Error' });
-  // }
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
-
-
-// router.post("/login", loginController);
 
 module.exports = router;
 
--- frontend/src/components/login/loginButton.js
+++ frontend/src/components/login/loginButton.js
@@ -6,55 +6,40 @@ function validateInput(loginData) {
 }
 
 const validateCredentials = async (loginData) => {
-    // try {
-    //     console.log(JSON.stringify(loginData));
-    //     const response = await axios.post("http://localhost:3001/users/test", "lol");
-
-    //     return response.data;
-    // } catch (error) {
-    //     console.error("Error validating credentials:", error);
-    //     return { isEmployee: false, isManager: false };
-    // }
-}
+    try {
+        const response = await axios.post('http://localhost:3001/users/login', loginData);
+        return response.data;
+    } catch (error) {
+        console.error("Error validating credentials:", error);
+        return { isEmployee: false, isManager: false };
+    }
+  }
+  
 
 export async function handleSubmit(event, loginData, setLoginMsg) {
     event.preventDefault();
-    console.log(JSON.stringify(loginData));
-    axios.post('http://localhost:3001/users/login', loginData)
-      .then(() => console.log('Book Created'))
-      .catch(err => {
-        console.error(err);
-    });
-}
-//     // const { isEmployee, isManager } = await validateCredentials(loginData);
-  
-//     // if (isManager) {
-//     //   const msg = (
-//     //     <div style={{ color: "green" }}>
-//     //       Manager
-//     //     </div>
-//     //   );
-  
-//     //   setLoginMsg(msg);
-//     // } else if (isEmployee) {
-//     //   const msg = (
-//     //     <div style={{ color: "green" }}>
-//     //       Employee
-//     //     </div>
-//     //   );
-  
-//     //   setLoginMsg(msg);
-//     // } else {
-//     //   const msg = (
-//     //     <div style={{ color: "red" }}>
-//     //       Invalid email or password.
-//     //     </div>
-//     //   );
-  
-//     //   setLoginMsg(msg);
-//     // }
-//   }
-  
+    const { isEmployee, isManager } = await validateCredentials(loginData);
+    
+    if (isManager) {
+        setLoginMsg (
+            <div style={{ color: "green" }}>
+                Manager
+            </div>
+        );
+    } else if (isEmployee) {
+        setLoginMsg (
+            <div style={{ color: "green" }}>
+                Employee
+            </div>
+        );
+    } else {
+        setLoginMsg (
+            <div style={{ color: "red" }}>
+                Invalid email or password.
+            </div>
+        );
+    }
+} 
 
 export function LoginButton({ loginData }) {
     return (
