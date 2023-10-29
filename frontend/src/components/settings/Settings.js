f6b1e8ab039021e9796c27e094691e1f0c7fa3e7
lex23unwin <lex23unwin@gmail.com>
2023-10-29 16:40:31 -0500

    settings functionality

--- frontend/src/components/settings/Settings.js
+++ frontend/src/components/settings/Settings.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const Settings = () => {
+const Settings = () => 
+{
+  useEffect(() => {
+    const updateValue = (element, valueElement) => {
+      valueElement.textContent = element.value;
+      localStorage.setItem(element.id, element.value);
+    };
+
+    const displayContrast = document.querySelector('#display-contrast');
+    const fontSize = document.querySelector('#font-size');
+    const mouseSensitivity = document.querySelector('#mouse-sensitivity');
+
+    displayContrast.addEventListener('input', () => {
+      updateValue(displayContrast, document.getElementById('display-contrast-value'));
+    });
+
+    fontSize.addEventListener('input', () => {
+      updateValue(fontSize, document.getElementById('font-size-value'));
+    });
+
+    mouseSensitivity.addEventListener('input', () => {
+      updateValue(mouseSensitivity, document.getElementById('mouse-sensitivity-value'));
+    });
+
+    if (localStorage.getItem('display-contrast')) {
+      displayContrast.value = localStorage.getItem('display-contrast');
+      updateValue(displayContrast, document.getElementById('display-contrast-value'));
+    }
+
+    if (localStorage.getItem('font-size')) {
+      fontSize.value = localStorage.getItem('font-size');
+      updateValue(fontSize, document.getElementById('font-size-value'));
+    }
+
+    if (localStorage.getItem('mouse-sensitivity')) {
+      mouseSensitivity.value = localStorage.getItem('mouse-sensitivity');
+      updateValue(mouseSensitivity, document.getElementById('mouse-sensitivity-value'));
+    }
+  }, []);
+  
   return (
     <div className="menu-body">
       <header className="menu-header">Welcome to the Sweet Paris Cafe!</header>
