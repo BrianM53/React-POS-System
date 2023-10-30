4d4d9040ec64d46230102c0564676fce3411d797
James <jimmy57guandolo@gmail.com>
2023-10-30 10:08:33 -0500

    Fixed js path errors, mostly renaming and redoing About.js

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -2,53 +2,51 @@ import './About.css';
 
 function About() {
     return (
-        <div className="about-section">
-            <h1>About Us Page</h1>
-            <p>Some text about who we are and what we do.</p>
-            <p>Resize the browser window to see that this page is responsive by the way.</p>
-            
-            <h2 style={{textAlign: 'center'}}>Our Team</h2>
-            <div className="row">
-                <div className="column">
-                    <div className="card">
-                        <img src="/images/defaultProfile.png" alt="John" style={{width: '100%'}} />
-                        <div className="container">
-                            <h2>John Doe</h2>
-                            <p className="title">Role</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>john@example.com</p>
-                            <p><button className="button">Contact</button></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <img src="/images/defaultProfile.png" alt="John" style={{width: '100%'}} />
-                        <div className="container">
-                            <h2>John Doe</h2>
-                            <p className="title">Role</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>john@example.com</p>
-                            <p><button className="button">Contact</button></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <img src="/images/defaultProfile.png" alt="John" style={{width: '100%'}} />
-                        <div className="container">
-                            <h2>John Doe</h2>
-                            <p className="title">Role</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>john@example.com</p>
-                            <p><button className="button">Contact</button></p>
-                        </div>
-                    </div>
-                </div>
+        <><div className="about-section">
+        <h1>About Us Page</h1>
+        <p>Some text about who we are and what we do.</p>
+        <p>Resize the browser window to see that this page is responsive by the way.</p>
+
+        <h2 style={{ textAlign: 'center' }}>Our Team</h2>
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <img src="/images/defaultProfile.png" alt="John" style={{ width: '100%' }} />
+              <div className="container">
+                <h2>John Doe</h2>
+                <p className="title">Role</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>john@example.com</p>
+                <p><button className="button">Contact</button></p>
+              </div>
             </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <img src="/images/defaultProfile.png" alt="John" style={{ width: '100%' }} />
+              <div className="container">
+                <h2>John Doe</h2>
+                <p className="title">Role</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>john@example.com</p>
+                <p><button className="button">Contact</button></p>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <img src="/images/defaultProfile.png" alt="John" style={{ width: '100%' }} />
+              <div className="container">
+                <h2>John Doe</h2>
+                <p className="title">Role</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>john@example.com</p>
+                <p><button className="button">Contact</button></p>
+              </div>
+            </div>
+          </div>
         </div>
-      </nav>
-      <main className="menu-main-aboutus">
+      </div><><main className="menu-main-aboutus">
         <div className="menu-main-aboutus-header">
           About Us
         </div>
@@ -65,20 +63,19 @@ function About() {
             </p>
           </div>
         </div>
-      </main>
-      <footer className="menu-footer">
-        <div className="menu-footer-message">
-          Get in touch with us! Follow us on:
-        </div>
-        <div className="menu-footer-container">
-          <i className="fa-brands fa-square-instagram fa-2xl menu-footer-smlink" id="menu-footer-instagram"></i>
-          <i className="fa-brands fa-twitter fa-2xl menu-footer-smlink" id="menu-footer-twitter"></i>
-          <i className="fa-brands fa-facebook fa-2xl menu-footer-smlink" id="menu-footer-facebook"></i>
-          <i className="fa-brands fa-tiktok fa-2xl menu-footer-smlink" id="menu-footer-tiktok"></i>
-        </div>
-      </footer>
-    </div>
+      </main><footer className="menu-footer">
+            <div className="menu-footer-message">
+              Get in touch with us! Follow us on:
+            </div>
+            <div className="menu-footer-container">
+              <i className="fa-brands fa-square-instagram fa-2xl menu-footer-smlink" id="menu-footer-instagram"></i>
+              <i className="fa-brands fa-twitter fa-2xl menu-footer-smlink" id="menu-footer-twitter"></i>
+              <i className="fa-brands fa-facebook fa-2xl menu-footer-smlink" id="menu-footer-facebook"></i>
+              <i className="fa-brands fa-tiktok fa-2xl menu-footer-smlink" id="menu-footer-tiktok"></i>
+            </div>
+          </footer></></>
+    
   );
 };
 
-export default AboutUs;
+export default About;
