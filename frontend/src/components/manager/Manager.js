fdd560ac7776e34553c173d6c7baf13142028afc
James <jimmy57guandolo@gmail.com>
2023-10-30 10:38:16 -0500

    Updated push

--- frontend/src/components/manager/Manager.js
+++ frontend/src/components/manager/Manager.js
@@ -1,13 +1,26 @@
+import React from 'react';
 import './Manager.css';
 import LogoutButton from '../utility/logoutButton';
 
+
 function Manager() {
+    // Replace this with the actual code to generate tables or charts
+    console.log(`Generating ${reportType}`);
+  };
+
   return (
     <div className="Manager">
       <header className="Manager-header">
         <p>
           This is the Manager page.
         </p>
+        <div className="button-container">
+          <button onClick={() => generateReport('Sales Report')}>Sales Report</button>
+          <button onClick={() => generateReport('Excess Report')}>Excess Report</button>
+          <button onClick={() => generateReport('Restock Report')}>Restock Report</button>
+          <button onClick={() => generateReport('Sells Together')}>Sells Together</button>
+          <button onClick={() => generateReport('Usage Chart')}>Usage Chart</button>
+        </div>
         <LogoutButton />
       </header>
     </div>
