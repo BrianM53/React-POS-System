807fc241e3349a39781ed9b69197f4fea91ae824
Deric Le <rake@tamu.edu>
2023-10-31 22:48:48 -0500

    merge with Manager skeleton page

--- frontend/src/components/manager/Manager.js
+++ frontend/src/components/manager/Manager.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import './Manager.css';
 import LogoutButton from '../utility/logoutButton';
-
-
-
+import generateReport from './generateReport';
 
 function Manager() {
-    // Replace this with the actual code to generate tables or charts
-  
+  // Replace this with the actual code to generate tables or charts
+
   return (
     <div className="Manager">
-      <header className="Manager-header">
-        <p>
-          This is the Manager page.
-        </p>
-        <div className="button-container">
-          {/* <button onClick={() => generateReport('Sales Report')}>Sales Report</button>
-          <button onClick={() => generateReport('Excess Report')}>Excess Report</button>
-          <button onClick={() => generateReport('Restock Report')}>Restock Report</button>
-          <button onClick={() => generateReport('Sells Together')}>Sells Together</button>
-          <button onClick={() => generateReport('Usage Chart')}>Usage Chart</button> */}
-        </div>
-        <LogoutButton />
-      </header>
+      <div className="button-container">
+        <button onClick={() => generateReport('Sales Report')}>Sales Report</button>
+        <button onClick={() => generateReport('Excess Report')}>Excess Report</button>
+        <button onClick={() => generateReport('Restock Report')}>Restock Report</button>
+        <button onClick={() => generateReport('Sells Together')}>Sells Together</button>
+        <button onClick={() => generateReport('Usage Chart')}>Usage Chart</button>
+        <button onClick={() => generateReport('Add Employee')}>Add Employee</button>
+      </div>
+      <div className="Manager-content">
+        {/* Your tables and charts go here */}
+      </div>
+      <LogoutButton />
     </div>
   );
 }
 
-
 export default Manager;
+
+
+
+
+
