941d27047563b6bdf7f8f4ef83ee6e3d08dfe75f
jimmy57guandolo <jimmy57guandolo@gmail.com>
2023-11-07 05:23:30 -0600

    routes.js update

--- backend/routes/reports.js
+++ backend/routes/reports.js
@@ -22,13 +22,42 @@ router.post("/sales-report", (req, res) => {
     });
 });
 
-router.get("/excess-report", (req, res) => {
+router.get("/excess-report", (req, res) => { //REQ NOT BEING READ
     // Assuming you have a generateExcessReport function in your Report module
+    const startDate = req.body.startDate;
+    const endDate = req.body.endDate;
+
     Report.generateExcessReport((error, excessReportData) => {
         if (error) {
             res.status(500).json({ error: "Error generating excess report" });
         } else {
             res.json(excessReportData);
+        }
+    });
+});
+
+router.get("/restock-report", (res) => {//REQ NOT BEING READ
+    // Assuming you have a generateExcessReport function in your Report module
+    Report.generateRestockReport((error, restockReportData) => {
+        if (error) {
+            res.status(500).json({ error: "Error generating restock report" });
+        } else {
+            res.json(restockReportData);
+        }
+    });
+});
+
+router.post("/sells-together", (req, res) => {
+    // const startDate = '2022-10-06 06:22:00';
+    // const endDate = '2022-10-06 08:25:00';
+    const startDate = req.body.startDate;
+    const endDate = req.body.endDate;
+
+    Report.generateSellsTogether(startDate, endDate, (error, sellsTogetherData) => {
+        if (error) {
+            res.status(500).json({ error: "Error generating sells together report" });
+        } else {
+            res.json(sellsTogetherData);
         }
     });
 });
