99b72e5aec9209ae5c036f1daaa35f487069379e
jimmy57guandolo <jimmy57guandolo@gmail.com>
2023-11-06 21:08:07 -0600

    Restock and Excess Report.js and queries

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -27,10 +27,64 @@ class Report {
         );
     }
 
-    static generateRestockReport(startDate, endDate, callback) {
+    static generateRestockReport(callback) {
+        connection.query(
+            "SELECT products.product_name, inventory.inventory_item, inventory.stock_level, inventory.restock_level, inventory.measurement_type \r\n" + //
+                    "FROM inventory\r\n" + //
+                    "INNER JOIN product_ingredients\r\n" + //
+                    "ON inventory.inventory_id = product_ingredients.inventory_id\r\n" + //
+                    "INNER JOIN products\r\n" + //
+                    "ON product_ingredients.product_id = products.product_id\r\n" + //
+                    "WHERE stock_level < restock_level;\r\n", //
+                    
+            (error, results) => {
+            if (error) {
+                return callback(error);
+            }
+            callback(null, results.rows);
+            }
+        );
     }
 
     static generateExcessReport(startDate, endDate, callback) {
+        connection.query(
+            "WITH usedStock AS (\r\n" + //
+                    "    SELECT\r\n" + //
+                    "        inventory.inventory_id,\r\n" + //
+                    "        inventory.inventory_item,\r\n" + //
+                    "        SUM(CASE WHEN orders.date_time >= ? AND orders.date_time <= ? THEN order_details.quantity * product_ingredients.quantity ELSE 0 END) AS total_used_between_timestamps\r\n" + //
+                    "    FROM inventory\r\n" + //
+                    "    INNER JOIN product_ingredients ON inventory.inventory_id = product_ingredients.inventory_id\r\n" + //
+                    "    INNER JOIN order_details ON product_ingredients.product_id = order_details.product_id\r\n" + //
+                    "    INNER JOIN orders ON order_details.order_id = orders.order_id\r\n" + //
+                    "    WHERE orders.date_time >= ?\r\n" + //
+                    "    GROUP BY inventory.inventory_id, inventory.inventory_item\r\n" + //
+                    "),\r\n" + //
+                    "\r\n" + //
+                    "currentStock AS (\r\n" + //
+                    "    SELECT\r\n" + //
+                    "        inventory.inventory_id,\r\n" + //
+                    "        SUM(inventory.stock_level) AS current_stock\r\n" + //
+                    "    FROM inventory\r\n" + //
+                    "    GROUP BY inventory.inventory_id\r\n" + //
+                    ")\r\n" + //
+                    "SELECT\r\n" + //
+                    "    I.inventory_item,\r\n" + //
+                    "    I.total_used_between_timestamps AS total_consumed,\r\n" + //
+                    "    C.current_stock AS current_stock,\r\n" + //
+                    "    (I.total_used_between_timestamps + C.current_stock) AS past_stock,\r\n" + //
+                    "    ((I.total_used_between_timestamps * 1.0) / (I.total_used_between_timestamps + C.current_stock)) * 100 AS percent_sold\r\n" + //
+                    "FROM usedStock AS I\r\n" + //
+                    "INNER JOIN currentStock AS C ON I.inventory_id = C.inventory_id\r\n" + //
+                    "WHERE ((I.total_used_between_timestamps * 1.0) / (I.total_used_between_timestamps + C.current_stock)) * 100 < 10;\r\n",
+            [startDate, endDate, startDate],
+            (error, results) => {
+            if (error) {
+                return callback(error);
+            }
+            callback(null, results.rows);
+            }
+        );
     }
 }
 
