a2bc0aeda8eccbcc118644d2a68b9170495aae37
jimmy57guandolo <jimmy57guandolo@gmail.com>
2023-11-09 22:54:37 -0600

    restock table

--- frontend/src/components/manager/generateReport.js
+++ frontend/src/components/manager/generateReport.js
@@ -3,15 +3,15 @@ function generateReport(reportType, data, setTableData) {
     setTableData(data);
 
   } else if (reportType == 'Excess Report') {
-
+    
   } else if (reportType == 'Restock Report') {
-
+    setTableData(data); // sets for only things to restock
   } else if (reportType == 'What Sells Together') {
 
   } else if (reportType == 'Usage Chart') {
 
   } else if (reportType == 'Add Employee') {
-    
+
   }
 }
   
