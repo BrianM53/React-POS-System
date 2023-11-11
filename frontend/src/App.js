6e392a09d4f230071d3d48452e3e06ca55e88f02
lex23unwin <lex23unwin@gmail.com>
2023-11-11 07:38:23 -0600

    about us fixed for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,39 +24,28 @@ const App = () => {
         <NavOptions />
       </nav>
       <main className="menu-main">
-      <section className="menu-main-section-1">
-        <h1 className="menu-main-section-1-h1">
-          Let Them Eat Crepes!
-        </h1>
-        <p className="menu-main-section-1-p">
-          Welcome to Sweet Paris' customer order interface! Feel free to browse the Menu by clicking the Menu tab, or check out the About Us and Contact Us pages if you're interested. Or, if you're ready to order, click the Order Now! button below to get started! Fresh crepes are just a few clicks away!
-          <br /> {}
-          - 
-          <br /> {}
-          Once you're on the menu page, go through each product category and click the plus button to add a product from that category to your cart. If you would like to remove them from your cart, you may click on the shopping cart icon above to edit your order.
-          Then, when you're finished, click on the shopping cart and click on the Pay Now! button and enter your details. Then, you're all set!
-        </p>
-      </section>
-      <aside className="menu-main-aside">
-        <img src="/sweetParisLocation.jpeg" alt="Sweet Crepes Logo" className="menu-main-aside-images" />
-        {}
-      </aside>
+        <section className="menu-main-section-1">
+          <h1 className="menu-main-section-1-h1">
+            Let Them Eat Crepes!
+          </h1>
+          <p className="menu-main-section-1-p">
+            Order Now
+          </p>
+        </section>
+        <aside className="menu-main-aside">
+          <img src="/sweetParisLocation.jpeg" alt="Sweet Crepes Logo" className="menu-main-aside-images" />
+          {}
+        </aside>
       </main>
-      <section className="menu-section-2">
-        <div className="menu-section-2-left">
-          <h3 className="menu-section-2-h3">Click Order Now to start your order!</h3>
-          <p className="menu-section-2-p">Once you sign in, you will have the ability to order any of the mouthwatering dishes! Click that button to the right or the sign-in button in the navigation bar above to build and submit your order!</p>
-        </div>
-      </section>
       <footer className="menu-footer">
         <div className="menu-footer-message">
           Get in touch with us! Follow us on:
         </div>
         <div className="menu-footer-container">
-          {/* <FontAwesomeIcon icon={['fab', 'instagram']} className="fa-2x" id="menu-footer-instagram" />
+          <FontAwesomeIcon icon={['fab', 'instagram']} className="fa-2x" id="menu-footer-instagram" />
           <FontAwesomeIcon icon={['fab', 'twitter']} className="fa-2x" id="menu-footer-twitter" />
           <FontAwesomeIcon icon={['fab', 'facebook']} className="fa-2x" id="menu-footer-facebook" />
-          <FontAwesomeIcon icon={['fab', 'tiktok']} className="fa-2x" id="menu-footer-tiktok" /> */}
+          <FontAwesomeIcon icon={['fab', 'tiktok']} className="fa-2x" id="menu-footer-tiktok" />
         </div>
       </footer>
     </div>
--- frontend/src/components/aboutUs/aboutUs.js
+++ frontend/src/components/aboutUs/aboutUs.js
@@ -1,80 +1,58 @@
+import React from 'react';
+import NavOptions from '../utility/navOptions';
+import { Link } from 'react-router-dom';
 import './aboutUs.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+library.add(fas,fab); 
 
 function AboutUs() {
     return (
-        <><div className="about-section">
-        <h1>About Us Page</h1>
-        <p>Some text about who we are and what we do.</p>
-        <p>Resize the browser window to see that this page is responsive by the way.</p>
-
-        <h2 style={{ textAlign: 'center' }}>Our Team</h2>
-        <div className="row">
-          <div className="column">
-            <div className="card">
-              <img src="/images/defaultProfile.png" alt="John" style={{ width: '100%' }} />
-              <div className="container">
-                <h2>John Doe</h2>
-                <p className="title">Role</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>john@example.com</p>
-                <p><button className="button">Contact</button></p>
-              </div>
+      <div className="menu-body">
+        <header className="menu-header">Welcome to the Sweet Paris Cafe!</header>
+        <nav className="menu-nav">
+          <Link to="/settings">
+              <FontAwesomeIcon icon={['fas', 'gear']} className="fa-2x" id="menu-nav-settings-icon" />
+          </Link>
+          <Link to="/app">
+              <FontAwesomeIcon icon={['fas', 'home']} className="fa-2x" id="menu-nav-home-icon" />
+          </Link>
+          <div className="menu-nav-title">Sweet Paris: Crepes and Cafe</div>
+          <NavOptions />
+        </nav>
+        <main className="menu-main-aboutus">
+          <div className="menu-main-aboutus-header">
+            About Us
+          </div>
+          <div className="menu-main-aboutus-container">
+            <div className="menu-main-aboutus-entry" id="menu-main-aboutus-entry-1">
+              <p>
+                Sweet Paris was first opened in Rice Village in 2012. Within 10 years, Sweet Paris has turned into one of the best fast casual hotspots in various parts of Houston, Austin, San Antonio, College Station, Miami, Woodbury, and international resort destinations.
+              </p>
+              <p>
+                Every day - and every bite - is an opportunity to savor all that's good in the world. Although crepes are known for being French, we have introduced hints of other cultures into this versatile dish that can be enjoyed any time of the day!
+              </p>
+              <p>
+                Now, after reading that last paragraph, you're probably pretty hungry. How can you get a taste of this celebrated cuisine? If you know what to order, click on either the Order Now! button on the home page, or click on the Order Now! button in the Menu after surveying the menu for a bit. Your choice!
+              </p>
             </div>
           </div>
-          <div className="column">
-            <div className="card">
-              <img src="/images/defaultProfile.png" alt="John" style={{ width: '100%' }} />
-              <div className="container">
-                <h2>John Doe</h2>
-                <p className="title">Role</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>john@example.com</p>
-                <p><button className="button">Contact</button></p>
-              </div>
-            </div>
+        </main>
+        <footer className="menu-footer">
+          <div className="menu-footer-message">
+            Get in touch with us! Follow us on:
           </div>
-          <div className="column">
-            <div className="card">
-              <img src="/images/defaultProfile.png" alt="John" style={{ width: '100%' }} />
-              <div className="container">
-                <h2>John Doe</h2>
-                <p className="title">Role</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>john@example.com</p>
-                <p><button className="button">Contact</button></p>
-              </div>
-            </div>
+          <div className="menu-footer-container">
+            <FontAwesomeIcon icon={['fab', 'instagram']} className="fa-2x" id="menu-footer-instagram" />
+            <FontAwesomeIcon icon={['fab', 'twitter']} className="fa-2x" id="menu-footer-twitter" />
+            <FontAwesomeIcon icon={['fab', 'facebook']} className="fa-2x" id="menu-footer-facebook" />
+            <FontAwesomeIcon icon={['fab', 'tiktok']} className="fa-2x" id="menu-footer-tiktok" />
           </div>
-        </div>
-      </div><><main className="menu-main-aboutus">
-        <div className="menu-main-aboutus-header">
-          About Us
-        </div>
-        <div className="menu-main-aboutus-container">
-          <div className="menu-main-aboutus-entry" id="menu-main-aboutus-entry-1">
-            <p>
-              Sweet Paris was first opened in Rice Village in 2012. Within 10 years, Sweet Paris has turned into one of the best fast casual hotspots in various parts of Houston, Austin, San Antonio, College Station, Miami, Woodbury, and international resort destinations.
-            </p>
-            <p>
-              Every day - and every bite - is an opportunity to savor all that's good in the world. Although crepes are known for being French, we have introduced hints of other cultures into this versatile dish that can be enjoyed any time of the day!
-            </p>
-            <p>
-              Now, after reading that last paragraph, you're probably pretty hungry. How can you get a taste of this celebrated cuisine? If you know what to order, click on either the Order Now! button on the home page, or click on the Order Now! button in the Menu after surveying the menu for a bit. Your choice!
-            </p>
-          </div>
-        </div>
-      </main><footer className="menu-footer">
-            <div className="menu-footer-message">
-              Get in touch with us! Follow us on:
-            </div>
-            <div className="menu-footer-container">
-              <i className="fa-brands fa-square-instagram fa-2xl menu-footer-smlink" id="menu-footer-instagram"></i>
-              <i className="fa-brands fa-twitter fa-2xl menu-footer-smlink" id="menu-footer-twitter"></i>
-              <i className="fa-brands fa-facebook fa-2xl menu-footer-smlink" id="menu-footer-facebook"></i>
-              <i className="fa-brands fa-tiktok fa-2xl menu-footer-smlink" id="menu-footer-tiktok"></i>
-            </div>
-          </footer></></>
-    
+        </footer>
+      </div>
   );
 };
 
