7a7e307bb0cba0721cf518b161fbf27b93550267
jimmy57guandolo <jimmy57guandolo@gmail.com>
2023-11-13 20:03:27 -0600

    charts

--- frontend/src/components/manager/Manager.js
+++ frontend/src/components/manager/Manager.js
@@ -11,18 +11,20 @@ import generateReport from './generateReport';
 import ReportButtons from './reportButtons';
 import ReportLabels from './reportLabels';
 import LogoutButton from '../utility/logoutButton';
+import { CChart } from '@coreui/react-chartjs';
 
 function Manager() {
   const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  
+
   // default variables are for Sales Report
   const [activeReport, setActiveReport] = useState(() => localStorage.getItem('activeReport') || 'Sales Report');
   const [columns, setColumns] = useState(['product_name1', 'product_name2', 'price', 'numsold', 'totalsales']);
 
   // the report data that will show up in the main content box
-  const [tableData, setTableData] = useState([]);
+
+  const [tableData, setTableData] = useState([]); //TODO: SET TABLE DATA!!!
 
   useEffect(() => {
     // Check if there's an active report in local storage and set it
@@ -35,6 +37,40 @@ function Manager() {
     return date_val + " " + time_val;
   }
 
+  function generateReport(reportType) {
+    console.log(reportType)
+    if (reportType === 'Sales Report') {
+      setColumns(['product_name', 'price', 'numsold', 'totalsales']);
+
+
+    } else if (reportType === 'Excess Report') {
+      setColumns(['inventory_item', 'total_consumed', 'current_stock', 'past_stock', 'percent_sold']);
+
+
+    } else if (reportType === 'Restock Report') {
+      setColumns(['inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
+
+
+    } else if (reportType === 'Sells Together') {
+      setColumns(['product_name1', 'product_name2', 'frequency']);
+
+
+    } else if (reportType === 'Usage Chart') {
+      setColumns(['product_name1', 'frequency']);
+
+    } else if (reportType === 'Add Employee') {
+
+    }
+
+  }
+
+
+  //=======================   Rounding loop for the data ==========================
+  // for(let i = 0; i<data.size(); i++){
+  //   data[i].total_consumed = data[i].total_consumed.toFixed(2);
+  // }
+
+
   function handleReport(e, reportType) {
     e.preventDefault();
 
@@ -44,15 +80,17 @@ function Manager() {
     const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
 
     let reportRoute = reportType.replace(" ", "-").toLowerCase();
-    
+
     axios
       .post(BACKEND_URL + '/reports/' + reportRoute, {
         startDate: generateTimestamp(startDate),
         endDate: generateTimestamp(endDate),
       })
       .then(response => {
-        console.log(reportType, "data for \nStart date:", generateTimestamp(startDate), "\nEnd date:", generateTimestamp(endDate), "\n", response.data.data); 
-        generateReport(reportType, response.data.data, setColumns, setTableData);
+        console.log(reportType, "data for \nStart date:", generateTimestamp(startDate), "\nEnd date:", generateTimestamp(endDate), "\n", response.data.data);
+        generateReport(reportType);
+        setTableData(response.data.data);
+        
       })
       .catch(error => {
         console.error('axios error:', error);
@@ -66,28 +104,71 @@ function Manager() {
 
         <div className="main-content">
           <table>
-            <thead className='content-head'> 
+            <thead className='content-head'>
             </thead>
             <tbody>
-              <canvas id="usageChart"></canvas> {/*not sure abt this one need to display chart*/}
+              {/* <canvas id="usageChart"></canvas> not sure abt this one need to display chart */}
               {tableData.map((element, index) => (
                 <tr key={index}>
                   {columns.map((column, columnIndex) => (
                     <td key={columnIndex}>{element[column]}</td>
                   ))}
                 </tr>
-              ))} 
+              ))}
             </tbody>
           </table>
         </div>
 
+        <CChart
+          type="bar"
+          data={{
+            labels: tableData,
+            datasets: [
+              {
+                label: "Usage Chart",
+                backgroundColor: "#ff0000",
+                data: columns,
+              }
+            ]
+          }}
+          labels="Inventory Items"
+          options={{
+            plugins: {
+              legend: {
+                labels: {
+                  color: "#00ffff",
+                }
+              }
+            },
+            scales: {
+              x: {
+                grid: {
+                  color: "#00ff00",
+                },
+                ticks: {
+                  color: "#0000ff",
+                },
+              },
+              y: {
+                grid: {
+                  color: "#ffcccc",
+                },
+                ticks: {
+                  color: "#000000",
+                },
+              },
+            },
+          }}
+        /> 
+
+
         <ReportButtons activeReport={activeReport} handleReport={handleReport} />
 
         <div className='date-btn'>
-          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} showTimeSelect timeFormat='HH:mm'/>
-          <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} showTimeSelect timeFormat='HH:mm'/>
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} showTimeSelect timeFormat='HH:mm' />
+          <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} showTimeSelect timeFormat='HH:mm' />
         </div>
-          
+
         <div className='utility-btn'>
           <Button onClick={() => navigate('/cashier')}>Cashier Interface</Button>
           <LogoutButton />
--- frontend/src/components/manager/generateReport.js
+++ frontend/src/components/manager/generateReport.js
@@ -1,81 +1,31 @@
-function generateReport(reportType, data, setColumns, setTableData) {
-  if (reportType === 'Sales Report') {
-    setColumns(['product_name', 'price', 'numsold', 'totalsales']);
+// function generateReport(reportType, data, setColumns, setTableData) {
+//   if (reportType === 'Sales Report') {
+//     setColumns(['product_name', 'price', 'numsold', 'totalsales']);
 
 
-  } else if (reportType === 'Excess Report') {
-    setColumns(['inventory_item', 'total_consumed', 'current_stock', 'past_stock', 'percent_sold']);
+//   } else if (reportType === 'Excess Report') {
+//     setColumns(['inventory_item', 'total_consumed', 'current_stock', 'past_stock', 'percent_sold']);
 
 
-  } else if (reportType === 'Restock Report') {
-    setColumns(['inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
-
-    
-  } else if (reportType === 'Sells Together') {
-    setColumns(['product_name1', 'product_name2', 'frequency']);
-
-
-  } else if (reportType === 'Usage Chart') {
-    renderUsageChart(data);
-    setColumns([]);
-    setTableData([]);
+//   } else if (reportType === 'Restock Report') {
+//     setColumns(['inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
 
     
-  } else if (reportType === 'Add Employee') {
+//   } else if (reportType === 'Sells Together') {
+//     setColumns(['product_name1', 'product_name2', 'frequency']);
+
+
+//   } else if (reportType === 'Usage Chart') {
     
-  }
+//   } else if (reportType === 'Add Employee') {
+    
+//   }
   
-  function renderUsageChart(data) {
-    // Extract labels and amounts from the data
-    const labels = data.map((item) => item.inventoryItem);
-    const amounts = data.map((item) => item.amountUsed);
-  
-    // Get the canvas element with the id "usageChart"
-    const ctx = document.getElementById('usageChart').getContext('2d');
-  
-    // Create a bar chart using Chart.js
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: 'Inventory items used',
-            data: amounts,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          x: {
-            type: 'category',
-            title: {
-              display: true,
-              text: 'Inventory Item',
-            },
-          },
-          y: {
-            type: 'linear',
-            position: 'left',
-            title: {
-              display: true,
-              text: 'Amount',
-            },
-          },
-        },
-      },
-    });
-  }
-
-}
-  
-export default generateReport;
-
-//=======================   Rounding loop for the data ==========================
-// for(let i = 0; i<data.size(); i++){
-//   data[i].total_consumed = data[i].total_consumed.toFixed(2);
 // }
+
+
+// //=======================   Rounding loop for the data ==========================
+// // for(let i = 0; i<data.size(); i++){
+// //   data[i].total_consumed = data[i].total_consumed.toFixed(2);
+// // }
  
