1a38aab0ae24cd8c23d4e5ff872010091132309e
Deric Le <rake@tamu.edu>
2023-11-14 20:23:10 -0600

    reword manager frontend itnerface

--- frontend/src/components/manager/Manager.js
+++ frontend/src/components/manager/Manager.js
@@ -11,7 +11,7 @@ import generateReport from './generateReport';
 import ReportButtons from './reportButtons';
 import ReportLabels from './reportLabels';
 import LogoutButton from '../utility/logoutButton';
-import { CChart } from '@coreui/react-chartjs';
+// import { CChart } from '@coreui/react-chartjs';
 
 function Manager() {
   const navigate = useNavigate();
@@ -25,8 +25,7 @@ function Manager() {
   // the report data that will show up in the main content box
 
   const [tableData, setTableData] = useState([]); 
-  const [showChart, setShowChart] = useState(false);
-  const [chartData, setChartData] = useState([])
+  // const [chartData, setChartData] = useState([])
   useEffect(() => {
     // Check if there's an active report in local storage and set it
     localStorage.setItem('activeReport', activeReport);
@@ -38,39 +37,38 @@ function Manager() {
     return date_val + " " + time_val;
   }
 
-  function generateReport(reportType) {
+  function generateReport(reportType, data) {
     console.log(reportType)
     if (reportType === 'Sales Report') {
       setColumns(['product_name', 'price', 'numsold', 'totalsales']);
 
+    } 
+    else if (reportType === 'Excess Report') {
+      for (let i = 0; i < data.length; i++) {
+        data[i].total_consumed = Math.round(data[i].total_consumed * 100) / 100;
+        data[i].percent_sold = Math.round(data[i].percent_sold * 100) / 100;
+      }
 
-    } else if (reportType === 'Excess Report') {
       setColumns(['inventory_item', 'total_consumed', 'current_stock', 'past_stock', 'percent_sold']);
+    } 
+    else if (reportType === 'Restock Report') {
+      setColumns(['product_name', 'inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
 
-
-    } else if (reportType === 'Restock Report') {
-      setColumns(['inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
-
-
-    } else if (reportType === 'Sells Together') {
+    } 
+    else if (reportType === 'Sells Together') {
       setColumns(['product_name1', 'product_name2', 'frequency']);
 
-
-    } else if (reportType === 'Usage Chart') {
+    } 
+    else if (reportType === 'Usage Chart') {
       setColumns(['product_name1', 'frequency']);
 
-    } else if (reportType === 'Add Employee') {
+    } 
+    else if (reportType === 'Add Employee') {
 
     }
 
+    setTableData(data)
   }
-
-
-  //=======================   Rounding loop for the data ==========================
-  // for(let i = 0; i<data.size(); i++){
-  //   data[i].total_consumed = data[i].total_consumed.toFixed(2);
-  // }
-
 
   function handleReport(e, reportType) {
     e.preventDefault();
@@ -89,18 +87,18 @@ function Manager() {
       })
       .then(response => {
         console.log(reportType, "data for \nStart date:", generateTimestamp(startDate), "\nEnd date:", generateTimestamp(endDate), "\n", response.data.data);
-        
-        if (reportType == "Usage Chart"){
-          setShowChart(true);
-          setChartData(response.data.data);
+        generateReport(reportType, response.data.data);
+        // if (reportType == "Usage Chart"){
+        //   setShowChart(true);
+        //   setChartData(response.data.data);
 
-          console.log("chartData:", chartData);//TODO Check data sent to chart
-        }
-        else{
-          setShowChart(false);
-          generateReport(reportType);
-          setTableData(response.data.data);
-        }
+        //   console.log("chartData:", chartData);//TODO Check data sent to chart
+        // }
+        // else{
+        //   setShowChart(false);
+        //   generateReport(reportType);
+        //   setTableData(response.data.data);
+        // }
       })
       .catch(error => {
         console.error('axios error:', error);
@@ -110,7 +108,9 @@ function Manager() {
   return (
     <div>
       <div className='Manager'>
-        <ReportLabels activeReport={activeReport} />
+        <div>
+          <ReportLabels activeReport={activeReport} />
+        </div>
 
         <div className="main-content">
           <table>
@@ -127,7 +127,7 @@ function Manager() {
             </tbody>
           </table>
         </div>
-        {showChart &&(
+        {/* {showChart &&(
           <CChart
             type="bar"
             data={{
@@ -169,7 +169,7 @@ function Manager() {
               },
             }}
           /> 
-        )}
+        )} */}
 
         <ReportButtons activeReport={activeReport} handleReport={handleReport} />
 
--- frontend/src/components/manager/generateReport.js
+++ frontend/src/components/manager/generateReport.js
@@ -1,27 +1,30 @@
-// function generateReport(reportType, data, setColumns, setTableData) {
-//   if (reportType === 'Sales Report') {
-//     setColumns(['product_name', 'price', 'numsold', 'totalsales']);
+// moved to manager.js
+
+function generateReport(reportType, data, setColumns) {
+  if (reportType === 'Sales Report') {
+    setColumns(['product_name', 'price', 'numsold', 'totalsales']);
 
 
-//   } else if (reportType === 'Excess Report') {
-//     setColumns(['inventory_item', 'total_consumed', 'current_stock', 'past_stock', 'percent_sold']);
+  } else if (reportType === 'Excess Report') {
+    setColumns(['inventory_item', 'total_consumed', 'current_stock', 'past_stock', 'percent_sold']);
 
 
-//   } else if (reportType === 'Restock Report') {
-//     setColumns(['inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
+  } else if (reportType === 'Restock Report') {
+    setColumns(['inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
 
     
-//   } else if (reportType === 'Sells Together') {
-//     setColumns(['product_name1', 'product_name2', 'frequency']);
+  } else if (reportType === 'Sells Together') {
+    setColumns(['product_name1', 'product_name2', 'frequency']);
 
 
-//   } else if (reportType === 'Usage Chart') {
+  } else if (reportType === 'Usage Chart') {
     
-//   } else if (reportType === 'Add Employee') {
+  } else if (reportType === 'Add Employee') {
     
-//   }
+  }
   
-// }
+}
+
 
 
 // //=======================   Rounding loop for the data ==========================
