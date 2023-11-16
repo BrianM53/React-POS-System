76c723834bb4b2c5f24a565c0f5660a960e1614f
oreogunleye24 <oreogunleye24@gmail.com>
2023-11-16 15:17:55 -0600

    Translate API changes

--- frontend/src/components/manager/Manager.js
+++ frontend/src/components/manager/Manager.js
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 import DatePicker from "react-datepicker";
-import axios from 'axios';
+import axios from "axios";
 
-import './Manager.css';
+import "./Manager.css";
 import "react-datepicker/dist/react-datepicker.css";
 
-// import generateReport from './generateReport';
-import ReportButtons from './reportButtons';
-import ReportLabels from './reportLabels';
-import LogoutButton from '../utility/logoutButton';
-import AddEmployee from './addEmployee';
-// import { CChart } from '@coreui/react-chartjs';
+import generateReport from "./generateReport";
+import ReportButtons from "./reportButtons";
+import ReportLabels from "./reportLabels";
+import LogoutButton from "../utility/logoutButton";
 
 function Manager() {
   const navigate = useNavigate();
@@ -20,20 +18,23 @@ function Manager() {
   const [endDate, setEndDate] = useState(new Date());
 
   // default variables are for Sales Report
-  const [activeReport, setActiveReport] = useState(() => localStorage.getItem('activeReport') || 'Sales Report');
-  const [columns, setColumns] = useState(['product_name1', 'product_name2', 'price', 'numsold', 'totalsales']);
+  const [activeReport, setActiveReport] = useState(
+    () => localStorage.getItem("activeReport") || "Sales Report"
+  );
+  const [columns, setColumns] = useState([
+    "product_name",
+    "price",
+    "numsold",
+    "totalsales",
+  ]);
 
   // the report data that will show up in the main content box
+  const [tableData, setTableData] = useState([]);
 
-  const [tableData, setTableData] = useState([]); 
-  // const [chartData, setChartData] = useState([])
   useEffect(() => {
-    localStorage.setItem('activeReport', activeReport);
+    // Check if there's an active report in local storage and set it
+    localStorage.setItem("activeReport", activeReport);
   }, [activeReport]);
-
-  useEffect(() => {
-    document.body.style.zoom = "100%";
-  }, []);
 
   function generateTimestamp(date) {
     const date_val = date.toISOString().slice(0, 10).replace("T", " ");
@@ -41,88 +42,52 @@ function Manager() {
     return date_val + " " + time_val;
   }
 
-  function generateReport(reportType, data) {
-    console.log(reportType)
-    if (reportType === 'Sales Report') {
-      setColumns(['product_name', 'price', 'numsold', 'totalsales']);
-      setTableData(data)
-    } 
-    else if (reportType === 'Excess Report') {
-      for (let i = 0; i < data.length; i++) {
-        data[i].total_consumed = Math.round(data[i].total_consumed * 100) / 100;
-        data[i].percent_sold = Math.round(data[i].percent_sold * 100) / 100;
-      }
-      setColumns(['inventory_item', 'total_consumed', 'current_stock', 'past_stock', 'percent_sold']);
-      setTableData(data)
-    } 
-    else if (reportType === 'Restock Report') {
-      setColumns(['product_name', 'inventory_item', 'stock_level', 'restock_level', 'measurement_type']);
-      setTableData(data)
-    } 
-    else if (reportType === 'Sells Together') {
-      setColumns(['product_name1', 'product_name2', 'frequency']);
-      setTableData(data)
-    } 
-    else if (reportType === 'Usage Chart') {
-      setColumns([]);
-    } 
-    else if (reportType === 'Add Employee') {
-      setColumns([]);
-      
-    }
-  }
-
   function handleReport(e, reportType) {
     e.preventDefault();
 
-    localStorage.setItem('activeReport', reportType);
+    localStorage.setItem("activeReport", reportType);
     setActiveReport(reportType);
 
-    const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
+    const BACKEND_URL =
+      process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
 
     let reportRoute = reportType.replace(" ", "-").toLowerCase();
 
-    if (reportRoute != "add-employee") {
-      axios
-        .post(BACKEND_URL + '/reports/' + reportRoute, {
-          startDate: generateTimestamp(startDate),
-          endDate: generateTimestamp(endDate),
-        })
-        .then(response => {
-          console.log(reportType, "data for \nStart date:", generateTimestamp(startDate), "\nEnd date:", generateTimestamp(endDate), "\n", response.data.data);
-          generateReport(reportType, response.data.data);
-          // if (reportType == "Usage Chart"){
-          //   setShowChart(true);
-          //   setChartData(response.data.data);
-  
-          //   console.log("chartData:", chartData);//TODO Check data sent to chart
-          // }
-          // else{
-          //   setShowChart(false);
-          //   generateReport(reportType);
-          //   setTableData(response.data.data);
-          // }
-        })
-        .catch(error => {
-          console.error('axios error:', error);
-        });
-    } else {
-      setColumns([]);
-    }
+    axios
+      .post(BACKEND_URL + "/reports/" + reportRoute, {
+        startDate: generateTimestamp(startDate),
+        endDate: generateTimestamp(endDate),
+      })
+      .then((response) => {
+        console.log(
+          reportType,
+          "data for \nStart date:",
+          generateTimestamp(startDate),
+          "\nEnd date:",
+          generateTimestamp(endDate),
+          "\n",
+          response.data.data
+        );
+        generateReport(
+          reportType,
+          response.data.data,
+          setColumns,
+          setTableData
+        );
+      })
+      .catch((error) => {
+        console.error("axios error:", error);
+      });
   }
 
   return (
     <div>
-      <div className='Manager'>
-        <div>
-          <ReportLabels activeReport={activeReport} />
-        </div>
+      <div className="Manager">
+        <ReportLabels activeReport={activeReport} />
 
         <div className="main-content">
-
-          <table className='main-content-table'>
-            <thead className='content-head'>
-            </thead>
+          <table>
+            <thead className="content-head"></thead>
             <tbody>
               {tableData.map((element, index) => (
                 <tr key={index}>
@@ -133,65 +98,34 @@ function Manager() {
               ))}
             </tbody>
           </table>
-          {activeReport === 'Add Employee' && <AddEmployee />}
-
-        </div>
-        {/* {showChart &&(
-          <CChart
-            type="bar"
-            data={{
-              labels: chartData,
-              datasets: [
-                {
-                  label: "Usage Chart",
-                  backgroundColor: "#ff0000",
-                  data: chartData,
-                }
-              ]
-            }}
-            labels="Inventory Items"
-            options={{
-              plugins: {
-                legend: {
-                  labels: {
-                    color: "#00ffff",
-                  }
-                }
-              },
-              scales: {
-                x: {
-                  grid: {
-                    color: "#00ff00",
-                  },
-                  ticks: {
-                    color: "#0000ff",
-                  },
-                },
-                y: {
-                  grid: {
-                    color: "#ffcccc",
-                  },
-                  ticks: {
-                    color: "#000000",
-                  },
-                },
-              },
-            }}
-          /> 
-        )} */}
-
-        <ReportButtons activeReport={activeReport} handleReport={handleReport} />
-
-        <div className='date-btn'>
-          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} showTimeSelect timeFormat='HH:mm' />
-          <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} showTimeSelect timeFormat='HH:mm' />
         </div>
 
-        <div className='utility-btn'>
-          <Button onClick={() => navigate('/cashier')}>Cashier Interface</Button>
+        <ReportButtons
+          activeReport={activeReport}
+          handleReport={handleReport}
+        />
+
+        <div className="date-btn">
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            showTimeSelect
+            timeFormat="HH:mm"
+          />
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            showTimeSelect
+            timeFormat="HH:mm"
+          />
+        </div>
+
+        <div className="utility-btn">
+          <Button onClick={() => navigate("/cashier")}>
+            Cashier Interface
+          </Button>
           <LogoutButton />
         </div>
-
       </div>
     </div>
   );
