62384bc7aeeb37db9e49fa57db275dafcefa9cee
Brian McKeown <brian.53.mckeown@gmail.com>
2023-11-26 19:15:12 -0600

    submits orders properly

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 var reportsRouter = require('./routes/reports');
+var ordersRouter = require('./routes/orders');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,6 +35,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/reports', reportsRouter);
+app.use('/orders', ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
