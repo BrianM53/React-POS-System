71722c14943e8e860c68ff32a14b5784c314ac0a
Deric Le <rake@tamu.edu>
2023-11-27 14:36:27 -0600

    fix the report label positiosn

--- frontend/src/components/manager/Manager.js
+++ frontend/src/components/manager/Manager.js
@@ -109,13 +109,47 @@ function Manager() {
 
   return (
     <div>
+      
       <div className='Manager'>
-        <div>
-          <ReportLabels activeReport={activeReport} />
+
+
+        {/* ================ SORRY FOR THIS MESS, COULDN'T FORMAT IT CORRECTLY AS A COMPONENT ================== */}
+        <div className={activeReport === "Sales Report" ? 'label-container' : 'passive-label'}>
+            <div className='label-item'>Menu Item</div>
+            <div className='label-item'>Price</div>
+            <div className='label-item'>Amount Sold</div>
+            <div className='label-item'>Total sales</div>
+        </div>
+        <div className={activeReport === "Excess Report" ? 'label-container' : 'passive-label'}>
+            <div className='label-item'>Ingredient</div>
+            <div className='label-item'>Total Consumed</div>
+            <div className='label-item'>Current Stock</div>
+            <div className='label-item'>Past Stock</div>
+            <div className='label-item'>% Sold</div>
+        </div>
+        <div className={activeReport === "Restock Report" ? 'label-container' : 'passive-label'}>
+            <div className='label-item'>Menu Item</div>
+            <div className='label-item'>Ingredient</div>
+            <div className='label-item'>Stock Level</div>
+            <div className='label-item'>Restock Level</div>
+            <div className='label-item'>Measurement Type</div>
+        </div>
+        <div className={activeReport === "Sells Together" ? 'label-container' : 'passive-label'}>
+            <div className='label-item'>First Menu Item</div>
+            <div className='label-item'>Second Menu Item</div>
+            <div className='label-item'>Sold Together</div>
+        </div>
+        <div className={activeReport === "Usage Chart" ? 'label-container' : 'passive-label'}>
+            <div className='label-item'>Amount of Ingredients Used</div>
+        </div>
+        <div className={activeReport === "Add Employee" ? 'label-container' : 'passive-label'}>
+          <div className='label-item'></div>
         </div>
 
-        <div className="main-content">
 
+        
+
+        <div className="main-content">
           <table className='main-content-table'>
             <thead className='content-head'>
             </thead>
--- frontend/src/components/manager/reportLabels.js
+++ frontend/src/components/manager/reportLabels.js
@@ -1,70 +1,48 @@
-import React from 'react';
+// import React from 'react';
 
-function ReportLabels({ activeReport }) {
-  return (
-    <div className='label-container'>
-      <div className={activeReport === "Sales Report" ? 'active-label' : 'passive-label'}>
-          <nav className='report-label'>
-            <ul>
-              <li><a className='label-item'>Menu Item</a></li>
-              <li><a className='label-item'>Price</a></li>
-              <li><a className='label-item'>Amount Sold</a></li>
-              <li><a className='label-item'>Total sales</a></li>
-            </ul>
-          </nav>
-        </div>
+// function ReportLabels({ activeReport }) {
+//   return (
+//     <div className='label-container'>
 
-        <div className={activeReport === "Excess Report" ? 'active-label' : 'passive-label'}>
-          <nav className='report-label'>
-            <ul>
-              <li><a className='label-item'>Ingredient</a></li>
-              <li><a className='label-item'>Total Consumed</a></li>
-              <li><a className='label-item'>Current Stock</a></li>
-              <li><a className='label-item'>Past Stock</a></li>
-              <li><a className='label-item'>% Sold</a></li>
-            </ul>
-          </nav>
-        </div>
+//         <div className={activeReport === "Sales Report" ? 'label-container' : 'passive-label'}>
+//             <div className='label-item'>Menu Item</div>
+//             <div className='label-item'>Price</div>
+//             <div className='label-item'>Amount Sold</div>
+//             <div className='label-item'>Total sales</div>
+//         </div>
 
-        <div className={activeReport === "Restock Report" ? 'active-label' : 'passive-label'}>
-          <nav className='report-label'>
-            <ul>
-              <li><a className='label-item'>Menu Item</a></li>
-              <li><a className='label-item'>Ingredient</a></li>
-              <li><a className='label-item'>Stock Level</a></li>
-              <li><a className='label-item'>Restock Level</a></li>
-              <li><a className='label-item'>Measurement Type</a></li>
-            </ul>
-          </nav>
-        </div>
+//         <div className={activeReport === "Excess Report" ? 'label-container' : 'passive-label'}>
+//             <div className='label-item'>Ingredient</div>
+//             <div className='label-item'>Total Consumed</div>
+//             <div className='label-item'>Current Stock</div>
+//             <div className='label-item'>Past Stock</div>
+//             <div className='label-item'>% Sold</div>
+//         </div>
 
-        <div className={activeReport === "Sells Together" ? 'active-label' : 'passive-label'}>
-          <nav className='report-label'>
-            <ul>
-              <li><a className='label-item'>First Menu Item</a></li>
-              <li><a className='label-item'>Second Menu Item</a></li>
-              <li><a className='label-item'>Sold Together</a></li>
-            </ul>
-          </nav>
-        </div>
+      
+//         <div className={activeReport === "Restock Report" ? 'label-container' : 'passive-label'}>
+//             <div className='label-item'>Menu Item</div>
+//             <div className='label-item'>Ingredient</div>
+//             <div className='label-item'>Stock Level</div>
+//             <div className='label-item'>Restock Level</div>
+//             <div className='label-item'>Measurement Type</div>
+//         </div>
 
-        <div className={activeReport === "Usage Chart" ? 'active-label' : 'passive-label'}>
-          <nav className='report-label'>
-            <ul>
-              <li><a className='label-item'>Amount of Ingredients Used</a></li>
-            </ul>
-          </nav>
-        </div>
+//         <div className={activeReport === "Sells Together" ? 'label-container' : 'passive-label'}>
+//             <div className='label-item'>First Menu Item</div>
+//             <div className='label-item'>Second Menu Item</div>
+//             <div className='label-item'>Sold Together</div>
+//         </div>
+      
+//         <div className={activeReport === "Usage Chart" ? 'label-container' : 'passive-label'}>
+//             <div className='label-item'>Amount of Ingredients Used</div>
+//         </div>
+      
+//         <div className={activeReport === "Add Employee" ? 'label-container' : 'passive-label'}>
+//           <div className='label-item'></div>
+//         </div>
+//     </div>
+//   );
+// }
 
-        <div className={activeReport === "Add Employee" ? 'active-label' : 'passive-label'}>
-          <nav className='report-label'>
-            <ul>
-              <li><a className='label-item'></a></li>
-            </ul>
-          </nav>
-        </div>
-    </div>
-  );
-}
-
-export default ReportLabels;
+// export default ReportLabels;
