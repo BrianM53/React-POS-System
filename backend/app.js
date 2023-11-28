c95752b2c692725771aee42c28fbffeb388f1864
oreogunleye24 <oreogunleye24@gmail.com>
2023-11-28 13:08:05 -0600

    Changes to add employee to database in frontend

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/reports", reportsRouter);
-app.use("./employees", employeesRouter);
+app.use("/employees", employeesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -3,20 +3,32 @@ var router = express.Router();
 const Employee = require("../models/Employee");
 
 router.post("/", (req, res) => {
-  const { firstName, lastName, phone, email, username, password } = req.body;
-  Employee.addEmployee(
-    firstName,
-    lastName,
-    phone,
-    email,
-    username,
-    password,
-    (error, result) => {
-      if (error) {
-        return res.status(500).json({ error: "Error adding employee" });
-      }
-      res.json({ message: "Employee added successfully" });
+  const { first_name, last_name, phone, email, username, password } = req.body;
+  Employee.emailExists(email, (error, exists) => {
+    if (error) {
+      return res
+        .status(500)
+        .json({ error: "Error checking employee existence" });
     }
-  );
+    if (exists) {
+      return res.status(409).json({ message: "Employee already exists" });
+    } else {
+      Employee.addEmployee(
+        first_name,
+        last_name,
+        phone,
+        email,
+        username,
+        password,
+        (error, result) => {
+          if (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Error adding employee" });
+          }
+          res.status(201).json({ message: "Employee added successfully" });
+        }
+      );
+    }
+  });
 });
 module.exports = router;
