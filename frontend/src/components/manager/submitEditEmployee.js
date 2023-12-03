48d964141447633895e399112de783464abfee04
lex23unwin <lex23unwin@gmail.com>
2023-12-03 15:53:23 -0600

    submitEditEmployee.js set up to validate input

---
+++ frontend/src/components/manager/submitEditEmployee.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function validateInput(editedEmployeeData) {
+  return (
+    editedEmployeeData.email.length > 0 &&
+    editedEmployeeData.username.length > 0 &&
+    editedEmployeeData.first_name.length > 0 &&
+    editedEmployeeData.last_name.length > 0
+  );
+}
+
+function SubmitEditEmployee({ editedEmployeeData }) {
+  const isInputValid = validateInput(editedEmployeeData);
+
+  return (
+    <Button type="submit" variant="light" disabled={!isInputValid}>
+      Submit Edited Employee
+    </Button>
+  );
+}
+
+export default SubmitEditEmployee;
