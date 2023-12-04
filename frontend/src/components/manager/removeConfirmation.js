12da66cb185fbd32c3fc887ee9dc593cfdaf7d2f
lex23unwin <lex23unwin@gmail.com>
2023-12-04 10:53:12 -0600

    are you sure delete pop up looks nicer

--- frontend/src/components/manager/removeConfirmation.js
+++ frontend/src/components/manager/removeConfirmation.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./remove.css";
 
 function DeleteConfirmation({ onConfirm, onCancel }) {
   return (
     <div className="remove-confirmation">
       <div className="remove-question">Are you sure you want to remove?</div>
-      <button onClick={onConfirm} className="remove-btn">Remove</button>
-      <button onClick={onCancel} className="cancel-btn" >Cancel</button>
+      <div className="btn-container">
+        <button onClick={onCancel} className="cancel-btn" >Cancel</button>
+        <button onClick={onConfirm} className="remove-btn">Remove</button>
+      </div>
     </div>
   );
 }
