d871d1062e68a5d814fcb337c4546a008d228d03
BrianM53 <Brian.53.McKeown@gmail.com>
2023-12-05 15:38:31 -0600

    inventory works

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,20 +1,19 @@
 const connection = require("../connection");
-const Product = require("./Products");
 
 class Order {
   static async createOrder(orderId, employeeId, customerId, totalCost, paymentStatus, paymentMethod, cart) {
     try {
       const currentDate = new Date().toISOString();
-  
+
       const getMaxOrderIdQuery = `
         SELECT MAX(order_id) AS max_order_id FROM orders
       `;
       const maxOrderIdResult = await connection.query(getMaxOrderIdQuery);
       console.log("Max Order ID Result:", maxOrderIdResult.rows[0]);
-  
+
       const orderId = maxOrderIdResult.rows[0].max_order_id + 1;
       console.log("New Order ID:", orderId);
-  
+
       console.log("Values for Insert Query:");
       console.log("Order ID:", orderId);
       console.log("Date Time:", currentDate);
@@ -23,7 +22,7 @@ class Order {
       console.log("Total Cost:", totalCost);
       console.log("Payment Method:", paymentMethod);
       console.log("Payment Status:", paymentStatus);
-  
+
       const query = `
         INSERT INTO orders (order_id, date_time, employee_id, customer_id, total_cost, payment_method, payment_status) 
         VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING order_id
@@ -37,33 +36,61 @@ class Order {
         paymentMethod,
         paymentStatus,
       ]);
-  
+
       console.log("New Order Result:", newOrderResult.rows[0]);
-  
+
       for (const product of cart) {
         await this.addProductToOrder(newOrderResult.rows[0].order_id, product.product_id, product.quantity);
+        await this.updateInventory(product.product_id, product.quantity);
       }
-  
+
     } catch (error) {
       console.error("Error in createOrder:", error);
       throw error;
     }
   }
-  
-  
+
+
 
   // add a product to an order
   static async addProductToOrder(orderId, productId, quantity) {
-    
+
     try {
       const result = await connection.query(
         "INSERT INTO order_details (order_id, product_id, quantity) VALUES ($1, $2, $3)",
         [orderId, productId, quantity]
       );
-  
+
       return result.rows[0];
     } catch (error) {
       console.error("Error adding product to order:", error);
+      throw error;
+    }
+  }
+
+  static async updateInventory(productId, quantity) {
+    try {
+      const quantityAndID = await connection.query(
+        "SELECT quantity, inventory_id FROM product_ingredients WHERE product_id = $1",
+        [productId]
+      );
+  
+      for (const item of quantityAndID.rows) {
+        const inventoryId = item.inventory_id;
+        const stockLevelResult = await connection.query(
+          "SELECT stock_level FROM inventory WHERE inventory_id = $1",
+          [inventoryId]
+        );
+        const currentStockLevel = stockLevelResult.rows[0].stock_level;
+        const newStockLevel = parseInt(currentStockLevel - quantity * item.quantity);
+        
+        await connection.query(
+          "UPDATE inventory SET stock_level = $1 WHERE inventory_id = $2",
+          [newStockLevel, inventoryId]
+        );
+      }
+    } catch (error) {
+      console.error("Error updating inventory:", error);
       throw error;
     }
   }
@@ -161,7 +188,7 @@ class Order {
       }
     );
   }
-  
+
   toString() {
     return `Order ID: ${this.orderId}, Employee ID: ${this.employeeId}, Customer ID: ${this.customerId}, Total Cost: ${this.totalCost}, Payment Status: ${this.paymentStatus}`;
   }
